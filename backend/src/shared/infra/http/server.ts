e731e1d50b6cd668f12ae7b8b1d894a88feac0d0
Bruno Henrique <bruno@cbkdigital.com.br>
2020-07-09 19:37:32 -0400

    chore: Add AppError file

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
-
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
+
+import AppError from '@shared/errors/AppError';
 
 // import routes from './routes';
 
@@ -9,8 +10,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
 const port = 3333;
 
 app.listen(port, () => {
-  console.log(`Server running into port ${port}!`);
+  console.log(`🚀 Server running into port ${port}!`);
 });
