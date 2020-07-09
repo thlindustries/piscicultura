0b0927066e6a2be96d34d6f333dab25c7c868ed4
thlindustries <thlindustries23@gmail.com>
2020-07-08 21:02:07 -0300

    initinal structure

---
+++ README.md
@@ -0,0 +1,1 @@
+Notion: https://www.notion.so/thlindustries/Piscicultura-a3a59193811040d5ab2282011cc8561a
---
+++ backend/src/shared/infra/http/server.ts
@@ -0,0 +1,9 @@
+import 'reflect-metadata';
+import 'dotenv/config';
+import express from 'express';
+
+import cors from 'cors';
+
+// import routes from './routes';
+
+const app = express();
