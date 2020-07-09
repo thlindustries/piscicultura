a28ca388f419d664b8089c639bf1b0a298f81ab0
thlindustries <thlindustries23@gmail.com>
2020-07-09 20:39:33 -0300

    chore: routes

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -2,13 +2,13 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 
 import AppError from '@shared/errors/AppError';
-
-// import routes from './routes';
+import routes from './routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
