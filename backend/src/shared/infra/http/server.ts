fe1d42161cb00b0cda41dab8567e0273d95d442b
Bruno Henrique <bruno@cbkdigital.com.br>
2020-07-09 20:15:59 -0400

    chore: Config typeorm

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import AppError from '@shared/errors/AppError';
 import routes from './routes';
 
+import '@shared/infra/typeorm';
 import '@shared/container';
 
 const app = express();
