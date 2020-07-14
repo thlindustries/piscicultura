009e383a1beae3200cd8792d8f63f995cce8f2a3
thlindustries <thlindustries23@gmail.com>
2020-07-13 22:26:59 -0300

    feature: react-native project created

---
+++ mobile/src/pages/SignUp/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const SignUp: React.FC = () => {
+  return (
+    <View>
+      <Text>Esta é a página de SignUp</Text>
+    </View>
+  );
+};
+
+export default SignUp;
