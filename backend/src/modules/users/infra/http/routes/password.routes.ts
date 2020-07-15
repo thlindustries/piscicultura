c77ca448d07af2fc9229ccf423ac7a0d0b949acf
Bruno Henrique <bruno@cbkdigital.com.br>
2020-07-14 20:40:01 -0400

    chore: Create and config router for Reset Password funcionality

--- backend/src/modules/users/infra/http/routes/password.routes.ts
+++ backend/src/modules/users/infra/http/routes/password.routes.ts
@@ -2,12 +2,12 @@ import { Router } from 'express';
 import { celebrate, Segments, Joi } from 'celebrate';
 
 import ForgotPasswordController from '../controllers/ForgotPasswordController';
-// import ResetPasswordController from '../controllers/ResetPasswordController';
+import ResetPasswordController from '../controllers/ResetPasswordController';
 
 const passwordRoutes = Router();
 
 const forgotPasswordController = new ForgotPasswordController();
-// const resetPasswordController = new ResetPasswordController();
+const resetPasswordController = new ResetPasswordController();
 
 passwordRoutes.post(
   '/forgot',
@@ -19,16 +19,16 @@ passwordRoutes.post(
   forgotPasswordController.create,
 );
 
-// passwordRoutes.post(
-//   '/reset',
-//   celebrate({
-//     [Segments.BODY]: {
-//       token: Joi.string().uuid().required(),
-//       password: Joi.string().min(6).required(),
-//       confirmPassword: Joi.valid(Joi.ref('password')).required(),
-//     },
-//   }),
-//   resetPasswordController.create,
-// );
+passwordRoutes.post(
+  '/reset',
+  celebrate({
+    [Segments.BODY]: {
+      token: Joi.string().uuid().required(),
+      password: Joi.string().min(6).required(),
+      confirmPassword: Joi.valid(Joi.ref('password')).required(),
+    },
+  }),
+  resetPasswordController.create,
+);
 
 export default passwordRoutes;
