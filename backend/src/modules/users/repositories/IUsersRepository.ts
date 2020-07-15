fd8dd7f6a77d5211ee9b7223633698a495ada8f9
Bruno Henrique <bruno@cbkdigital.com.br>
2020-07-14 20:30:24 -0400

    chore: Add new two functions for UsersRepository

--- backend/src/modules/users/repositories/IUsersRepository.ts
+++ backend/src/modules/users/repositories/IUsersRepository.ts
@@ -4,6 +4,8 @@ import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 interface IUserRepository {
   create(data: ICreateUserDTO): Promise<User>;
   findByEmail(email: string): Promise<User | undefined>;
+  findById(id: string): Promise<User | undefined>;
+  save(user: User): Promise<User>;
 }
 
 export default IUserRepository;
