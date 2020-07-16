33ad42ed6f9b31d3c0de443913d5675f2e8f9d1c
Bruno Henrique <bruno@cbkdigital.com.br>
2020-07-15 20:25:36 -0400

    chore: Update mail config

--- backend/src/config/mail.ts
+++ backend/src/config/mail.ts
@@ -1,7 +1,21 @@
 interface IMailConfig {
   driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
 }
 
 export default {
   driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'thiago@thdev.dev',
+      name: 'FishBoard',
+    },
+  },
 } as IMailConfig;
