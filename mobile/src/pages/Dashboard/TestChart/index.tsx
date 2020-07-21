839ec916ae9b9bb65e5e226bf0a86ab859315527
thlindustries <thlindustries23@gmail.com>
2020-07-21 20:32:18 -0300

    chore: chart updated

--- mobile/src/pages/Dashboard/TestChart/index.tsx
+++ mobile/src/pages/Dashboard/TestChart/index.tsx
@@ -1,29 +1,3 @@
-// import React from 'react';
-// import { VictoryLine, VictoryChart, VictoryTheme } from 'victory-native';
-
-// import { Container } from './styles';
-
-// const data = [
-//   { x: 1, y: 13000 },
-//   { x: 2, y: 16500 },
-//   { x: 3, y: 14250 },
-//   { x: 4, y: 19000 },
-// ];
-
-// class TestingChart extends React.Component {
-//   render() {
-//     return (
-//       <Container>
-//         <VictoryChart width={350} theme={VictoryTheme.material}>
-//           <VictoryLine data={data} />
-//         </VictoryChart>
-//       </Container>
-//     );
-//   }
-// }
-
-// export default TestingChart;
-
 import React from 'react';
 
 import { Container } from './styles';
@@ -40,7 +14,7 @@ interface iData {
 }
 
 const TestChart: React.FC<iData> = ({ data }) => {
-  console.log(data);
+  //console.log(data);
   return (
     <Container>
       <VictoryChart width={300} height={300} theme={VictoryTheme.material}>
