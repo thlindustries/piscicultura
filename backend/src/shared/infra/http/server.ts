fabfe5035f74cbb12eba1431e6c7fee1646b8970
thlindustries <thlindustries23@gmail.com>
2020-07-22 22:16:11 -0300

    feature: socket.io added

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -5,6 +5,9 @@ import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 
+import io from 'socket.io';
+import http from 'http';
+
 import AppError from '@shared/errors/AppError';
 import routes from './routes';
 
@@ -12,6 +15,16 @@ import '@shared/infra/typeorm';
 import '@shared/container';
 
 const app = express();
+const server = http.createServer(app)
+const socket = io(server);
+
+socket.on('connection', () => {
+  console.log('A user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
 
 app.use(cors());
 app.use(express.json());
